6296ca695e6b02623e9e42e7376a9ecb42e1ddbd
Gloosovint <angelogonzalesteran@gmail.com>
2024-11-02 18:09:04 -0500

    Esta versión fue mejorada usando funciones para que el dado se lance varias veces

--- Lanza_los_dados.py
+++ Lanza_los_dados.py
@@ -1,4 +1,15 @@
 import random
 
-dado = random.randint(1,6)
-print(dado)
+def lanzar_dado():
+    return random.randint(1, 6)
+
+def simulacion_lanzamientos(cantidad_lanzamientos):
+    resultados = []
+    for _ in range(cantidad_lanzamientos):
+        resultado = lanzar_dado()
+        resultados.append(resultado)
+    return resultados
+
+cantidad_lanzamientos = int(input("¿Cuántas veces deseas lanzar el dado? "))
+resultados = simulacion_lanzamientos(cantidad_lanzamientos)
+print("Resultados de los lanzamientos:", resultados)
